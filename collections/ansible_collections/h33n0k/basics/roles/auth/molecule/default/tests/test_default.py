5f04b3199397712653db1ca279178e64c3c703d0
h33n0k <valentin@gorr.in>
2025-05-27 23:30:43 +0200

    feat(basics/auth): ssh login
    - Setup ssh server configuration
    - Setup ssh permissions
    - Add testinfra unit tests

--- collections/ansible_collections/h33n0k/basics/roles/auth/molecule/default/tests/test_default.py
+++ collections/ansible_collections/h33n0k/basics/roles/auth/molecule/default/tests/test_default.py
@@ -33,3 +33,38 @@ def test_users_creation(host, AnsibleVars):
         for group in user['groups']:
             assert group in u.groups, \
                 f"user {user['username']} should be in group {group}"
+
+
+def test_ssh_server_package(host):
+    package = host.package('openssh-server')
+    assert package.is_installed, \
+        'openssh-server should have been installed.'
+
+
+def test_ssh_rsyslog(host):
+    file = host.file('/etc/rsyslog.d/sshd.conf')
+    assert file.exists, \
+        'rsyslog configuration should have been deployed.'
+    assert file.user == 'root'
+    assert file.group == 'adm'
+    assert file.mode == 0o644
+
+
+def test_sshd_config(host):
+    file = host.file('/etc/ssh/sshd_config')
+    assert file.exists, \
+        'sshd configuration should have been deployed.'
+    assert file.user == 'root'
+    assert file.group == 'adm'
+    assert file.mode == 0o644
+
+
+def test_ssh_permissions(host, AnsibleVars):
+    users = AnsibleVars.get('auth_users')
+    for user in users:
+        if 'ssh' in user['groups']:
+            directory = host.file(f"/home/{user['username']}/.ssh")
+            assert directory.exists
+            assert directory.user == user['username']
+            assert directory.group == user['username']
+            assert directory.mode == 0o700
